a833e5df9c829ab69fae2b785d79e1b98dab87c2
milanwinter <milanwinter8@gmail.com>
2020-12-02 09:57:42 -0800

    added bulma, added table, add exercise button

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+    
     const exerciseTable = document.querySelector("tbody")
+    const exerciseDiv = document.querySelector("#exercise-panel")
+    exerciseDiv.addEventListener("click", (e) => handleEvent(e))
 
 
 
@@ -19,19 +22,33 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     function createNewExercise(ex) {
-        console.log(ex)
         const tr = document.createElement("tr")
         const name = document.createElement("td")
         const description = document.createElement("td")
         const muscleGroup = document.createElement("td")
-    
+        const addButton = document.createElement('button')
+        const imageCell = document.createElement("td")
+        const image = document.createElement("img")
+        image.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzcOouX0aFFrzVJIVLTibFcageIi5Rw5ySkA&usqp=CAU"
+        imageCell.appendChild(image)
+        addButton.innerText = "Add"
+        addButton.className = "button is-primary"
+        addButton.dataset.exerciseId= ex.id
         name.innerText = ex.name
         description.innerText = ex.description
         muscleGroup.innerText=ex.muscle_group.name
 
-        tr.append(name,description,muscleGroup)
+        tr.append(name,description,muscleGroup,imageCell,addButton)
 
         exerciseTable.appendChild(tr)
 
+    }
+
+    function handleEvent(e) {
+      
+        if(e.target.tagName === "BUTTON"){
+            const id = e.target.dataset.exerciseId
+            console.log(id)
+        }
     }
 })
